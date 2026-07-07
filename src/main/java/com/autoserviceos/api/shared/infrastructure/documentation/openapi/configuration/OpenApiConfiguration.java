ee9d553aef770d42adfa492b5083208bfa02bab8
Diego <luis.diegoa.03@gmail.com>
2026-07-07 01:07:40 -0500

    feat: add authorization bearer

--- src/main/java/com/autoserviceos/api/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/autoserviceos/api/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,12 +1,14 @@
 package com.autoserviceos.api.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 /**
  * Configuration class for OpenAPI (Swagger) documentation.
  * Defines the global metadata for the AutoServiceOS API.
@@ -16,14 +18,27 @@ public class OpenApiConfiguration {
 
     @Bean
     public OpenAPI autoServiceOpenApi() {
+        final String securitySchemeName = "Bearer Authentication";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("AutoServiceOS API")
                         .description("AutoServiceOS Platform application REST API documentation.")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                         .description("AutoServiceOS Platform Wiki Documentation")
-                        .url("https://autoserviceos.wiki.github.io/docs"));
+                        .url("https://autoserviceos.wiki.github.io/docs"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
     }
 }
